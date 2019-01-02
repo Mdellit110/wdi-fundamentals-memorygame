f9eaa26e44d77689302e92348c90f59a8413cf76
matthew dellitalia <matthewdellitalia@matthews-mbp.lan>
2019-01-02 10:09:57 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,19 @@
 console.log("up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[1]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+console.log("user flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("you found a match!");
+	} else {
+		alert("sorry try again!");
+	}
+}
